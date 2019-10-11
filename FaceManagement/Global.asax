dadefacbb3e84e4212ee2fa4e986358e7952e7c8
phamngocduy <phmngcduy@gmail.com>
2019-10-11 13:03:29 +0700

    Init project

---
+++ FaceManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FaceManagement.MvcApplication" Language="C#" %>
---
+++ FaceManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FaceManagement.Startup))]
+namespace FaceManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
