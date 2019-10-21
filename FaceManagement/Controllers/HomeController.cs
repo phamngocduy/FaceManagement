d8a8e1286682f4cde3f6323cda31bfc6bb94d917
phamngocduy <phmngcduy@gmail.com>
2019-10-21 22:33:04 +0700

    Add check in

--- FaceManagement/Controllers/HomeController.cs
+++ FaceManagement/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FaceManagement.Models;
 
 namespace FaceManagement.Controllers
 {
@@ -21,6 +23,25 @@ namespace FaceManagement.Controllers
         public ActionResult Contact()
         {
             return View();
+        }
+
+        public string Upload(string code, string user, HttpPostedFileBase face)
+        {
+            try
+            {
+                code = code.Split('-')[0];
+                using (var db = new FaceIDEntities())
+                    if (db.MyClasses.Find(int.Parse(code)) == null)
+                        throw new Exception("Class doesn't exist");
+                var path = "~/App_Data/Checks/";
+                Directory.CreateDirectory(Path.Combine(Server.MapPath(path), code));
+                face.SaveAs(Path.Combine(Server.MapPath(path), code, user + ".jpg"));
+                return "Check in successfully";
+
+            } catch (Exception e)
+            {
+                return e.GetBaseException().Message;
+            }
         }
     }
 }
